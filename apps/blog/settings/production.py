b358bc8c10f3179b2f06086f88f8163e65acc5ad
AinoHana <rossyuchiha@gmail.com>
2025-08-11 16:56:45 -0300

    production.py actualizado

--- apps/blog/settings/production.py
+++ apps/blog/settings/production.py
@@ -1,13 +1,32 @@
-from .base import *
+# settings/production.py
 
+from .base import *
+import os
+
+# Desactivar el modo de depuración para producción
 DEBUG = False
 
-ALLOWED_HOSTS = ['tu_dominio.com', 'www.tu_dominio.com', 'IP_de_tu_servidor'] # ¡IMPORTANTE: Cambiar en producción!
+# El dominio de tu aplicación en PythonAnywhere
+ALLOWED_HOSTS = ['AinoHana.pythonanywhere.com']
 
-# Configuración para static files en producción (puedes recolectarlos en un solo lugar)
-STATIC_ROOT = BASE_DIR / 'staticfiles' # Donde Django recolectará los archivos estáticos en producción
+# Cargar la SECRET_KEY desde una variable de entorno para mayor seguridad
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-fallback-key')
 
-# Si usas alguna solución de almacenamiento de archivos en la nube, configúrala aquí.
+# Configuración de los archivos estáticos y media
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Configuración de la base de datos para producción con SQLite 3
+# La base de datos debe estar en una carpeta fuera de la aplicación principal para evitar problemas de permisos
+DB_PATH = os.path.join(os.path.dirname(BASE_DIR), 'database')
+os.makedirs(DB_PATH, exist_ok=True)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DB_PATH, 'db.sqlite3'),
+    }
+}
 
 # Seguridad adicional para producción
 CSRF_COOKIE_SECURE = True
